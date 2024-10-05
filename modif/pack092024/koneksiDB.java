c6cea5b012ee6281e8f0d1848b6ad2a5f39473be
ryanbekabe <bekabeipa@gmail.com>
2024-10-05 08:30:56 +0700

    Add files via upload

--- modif/pack092024/koneksiDB.java
+++ modif/pack092024/koneksiDB.java
@@ -49,15 +49,15 @@ public class koneksiDB {
                         "  Informasi dan panduan bisa dicek di halaman https://github.com/mas-elkhanza/SIMRS-Khanza/wiki \n"+
                         "  Bagi yang ingin berdonasi untuk pengembangan aplikasi ini bisa ke BSI 1015369872 atas nama Windiarto\n"+
                         "  *Pengembangan/modifikasi pada:                          \n"+
-                        "  1) Form/Java: rekammedis/RMDataResumePasien.java (1) (Kondisi Pasien Pulang); simrskhanza/DlgIKBBayi.java (Tgl Lhr Ibu; Tgl Lhr Ayah, Surat Keterangan Lahir 1 - Tunggal; Surat Keterangan Lahir 2 - Kembar); simrskhanza/DlgPasienMati.java (1) (Ket./NoSrt, Surat Kematian 1 - Oke); surat/SuratButaWarna.java (1); surat/SuratKeteranganSehat.java (1); surat/SuratSakit.java (1) (Cetak Surat Sakit 1 - Oke); simrskhanza/DlgKasirRalan.java (Surat Keterangan Sehat 3 - Oke); simrskhanza/DlgReg.java (Surat Keterangan Sehat 3 - Oke); fungsi/sekuel.java (AKTIFKANTRACKSQL); simrskhanza/frmUtama.java;       \n"+
-                        "   ) Form/Java: ;    \n"+
-                        "  2) Report: rptSuratSakit5.jasper (1); rptSuratKeteranganSehat.jasper (1); rptSuratTidakButaWarna.jasper (1); rptLaporanResume.jasper (1); rptSKL1.jasper & rptSKL2.jasper; rptSuratKematian.jasper (1);     \n"+
+                        "  1) Form/Java: rekammedis/RMDataResumePasien.java (1) (Kondisi Pasien Pulang); simrskhanza/DlgIKBBayi.java (1) (Tgl Lhr Ibu; Tgl Lhr Ayah, Surat Keterangan Lahir 1 - Tunggal; Surat Keterangan Lahir 2 - Kembar); simrskhanza/DlgPasienMati.java (1) (Ket./NoSrt, Surat Kematian 1 - Oke); surat/SuratButaWarna.java (1); surat/SuratKeteranganSehat.java (1); surat/SuratSakit.java (1) (Cetak Surat Sakit 1 - Oke); simrskhanza/DlgKasirRalan.java (1) (Surat Keterangan Sehat 3 - Oke); simrskhanza/DlgReg.java (1) (Surat Keterangan Sehat 3 - Oke); fungsi/sekuel.java (1) (AKTIFKANTRACKSQL); simrskhanza/frmUtama.java (1) ;       \n"+
+                        "   ) Form/Java: inventory/DlgCopyResep.java (1) (Silakan konfirmasi ke bagian Farmasi); inventory/DlgResepObat.java (0); simrskhanza/DlgRawatJalan.java (1) (Suhu harap dilengkapi, dll...); rekammedis/RMPenilaianAwalMedisIGD.java (1) (V. DIAGNOSIS/ASESMEN DAN RETRIASE); simrskhanza/DlgAbout.java (1);   \n"+
+                        "  2) Report: rptSuratSakit5.jasper (1); rptSuratKeteranganSehat.jasper (1); rptSuratTidakButaWarna.jasper (1); rptLaporanResume.jasper (1); rptSKL1.jasper (1) & rptSKL2.jasper(1) (NIK Ibu); rptSuratKematian.jasper (1); rptCetakPenilaianAwalMedisIGD.jasper (1);     \n"+
                         "   ) Report: ;     \n"+
                         "  3) Database: ;     \n"+
-                        "  4) Perbaikan: ;     \n"+
-                        "  5) Tambahan: ;     \n"+
-                        "  Re-Compile: LM Cinnamon 5.4.12 i7 IDE 15 JDK bellsoft-java15-full-amd64; Git Clone 09 September 2024. \n"+
-                        "  Re-Compile_by: Bang Riyan Hidayat Samosir - www.HanyaJasa.Com - hanyajasa@gmail.com, 11.06 23/09/2024 - RS Islam PKU Muhammadiyah Palangka Raya. \n"+
+                        "  4) Perbaikan: Upload berkas rawat digital;     \n"+
+                        "  5) Tambahan: Web verifikasi QR Surat Keterangan; warna hijau [0,204,0]; SK Lahir NIP Ibu; TTV poli harus isi; SIP dokter; Lock obat verif;    \n"+
+                        "  Re-Compile: LM Cinnamon 5.4.12 i7 IDE 15 JDK bellsoft-java15-full-amd64 / MSI IDE 21 Liberica JDK 15 Full 64-bit; Git Clone 09 September 2024. \n"+
+                        "  Re-Compile_by: Bang Riyan Hidayat Samosir - www.HanyaJasa.Com - hanyajasa@gmail.com, 08.30 05/10/2024 - RS Islam PKU Muhammadiyah Palangka Raya. \n"+
                         "                                                                           ");
             }catch(Exception e){
                 System.out.println("Notif : "+e);
