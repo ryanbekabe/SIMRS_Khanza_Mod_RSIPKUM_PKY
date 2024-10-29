b3cac63a7866eab4ada7cef221625d3dc0739e5f
ryanbekabe <bekabeipa@gmail.com>
2024-10-29 08:41:19 +0700

    Add files via upload

--- modif/pack102024/DlgCariPeriksaLabPA.java
+++ modif/pack102024/DlgCariPeriksaLabPA.java
@@ -121,11 +121,11 @@ public class DlgCariPeriksaLabPA extends javax.swing.JDialog {
         kdmem.setDocument(new batasInput((byte)8).getKata(kdmem));
         kdptg.setDocument(new batasInput((byte)25).getKata(kdptg));
         TCari.setDocument(new batasInput((byte)100).getKata(TCari));
-        DiagnosaKlinis.setDocument(new batasInput((int)50).getKata(DiagnosaKlinis));
+        DiagnosaKlinis.setDocument(new batasInput((int)1024).getKata(DiagnosaKlinis));
         Makroskopis.setDocument(new batasInput((int)1024).getKata(Makroskopis));
         Mikroskopis.setDocument(new batasInput((int)1024).getKata(Mikroskopis));
-        Kesimpulan.setDocument(new batasInput((int)300).getKata(Kesimpulan));
-        Kesan.setDocument(new batasInput((int)300).getKata(Kesan));
+        Kesimpulan.setDocument(new batasInput((int)1024).getKata(Kesimpulan));
+        Kesan.setDocument(new batasInput((int)1024).getKata(Kesan));
         if(koneksiDB.CARICEPAT().equals("aktif")){
             TCari.getDocument().addDocumentListener(new javax.swing.event.DocumentListener(){
                 @Override
