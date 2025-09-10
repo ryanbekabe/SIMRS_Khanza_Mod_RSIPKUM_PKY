c3a371842838bd2b6004b0ddb9bfd17e80540d66
ryanbekabe <bekabeipa@gmail.com>
2025-09-10 14:30:20 +0700

    Add files via upload

--- modif/pack092025/koneksiDB.java
+++ modif/pack092025/koneksiDB.java
@@ -58,7 +58,7 @@ public class koneksiDB {
                         
                         "        inventory/DlgCopyResep.java (1) (Silakan konfirmasi ke bagian Farmasi);\n"+
                         "        inventory/DlgInputStok.java (0 lewati dulu) (Tambah kolom Ekspired);\n"+
-                        "        inventory/DlgPermintaanResepPulang.java (1) (Sembunyikan stok obat yang kosong 28082025);\n"+
+                        "        inventory/DlgPermintaanResepPulang.java (1) (Sembunyikan stok obat yang kosong, perbaiki doubling 28082025, 10092025);\n"+
                         "        inventory/DlgResepObat.java (1) (Tambah No.SEP di rptresep jika pasien BPJS, E-Tiket Obat);\n"+
                         "        inventory/DlgStokOpname.java (0 lewati dulu) (Tambah kolom Ekspired);\n"+
                         "        inventory/InventoryObatBHPTidakBergerak.java (0 lewati dulu) (Tambah kolom Ekspired);\n"+
@@ -66,7 +66,8 @@ public class koneksiDB {
                         "        kepegawaian/DlgDokter.java (1) (Perbaiki fungsi simpan dokter 09082025);\n"+
 
                         "        laporan/DlgRl38.java (1) (Tambah JK L P 13062025);\n"+
-                        "        laporan/DlgRL4A.java (0) ();\n"+
+                        "        laporan/DlgRL4A.java (1) (Sesuai SIRS RM salin dari Ichan 09092025);\n"+
+                        "        laporan/DlgRL4B.java (1) (Sesuai SIRS RM salin dari Ichan 09092025);\n"+
                         
                         "        permintaan/DlgPermintaanKonsultasiMedik.java (1) (KOP, SIP dr.);\n"+
                         
@@ -158,7 +159,7 @@ public class koneksiDB {
                         "        rptSuratTidakButaWarna.jasper (1);\n"+
                         
                         "  Re-Compile: LM Cinnamon 5.4.12 i7 IDE 15 JDK bellsoft-java15-full-amd64 / MSI IDE 21 Liberica Java Platform JDK 15 Full 64-bit Source/Binary Format JDK 8; Git Clone 22 Feb 2025. \n"+
-                        "  Re-Compile_by: Bang Sam - www.HanyaJasa.Com - hanyajasa@gmail.com, 09/09/2025 - RS Islam PKU Muhammadiyah Palangka Raya. \n"+
+                        "  Re-Compile_by: Bang Sam - www.HanyaJasa.Com - hanyajasa@gmail.com, 10/09/2025 - RS Islam PKU Muhammadiyah Palangka Raya. \n"+
                         "                                                                           ");
 
             }catch(Exception e){
